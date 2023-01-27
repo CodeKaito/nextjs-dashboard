48d291660114ae8ac01a1f38363bff877f9e40c6
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-01-27 11:31:41 +0100

    update dependencies

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div><h1 className="text-3xl font-bold underline text-red-800">
+    Hello world!
+</h1></div>
+  )
+}
+
+export default Header
