a999d5e12ee280ce284654ee22afbe68f10b3d94
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-01-27 14:10:39 +0100

    update colors

--- components/RecentOrders.jsx
+++ components/RecentOrders.jsx
@@ -12,8 +12,8 @@ const RecentOrders = () => {
             key={id}
             className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointer'
           >
-            <div className='bg-purple-100 rounded-lg p-3'>
-              <FaShoppingBag className='text-purple-800' />
+            <div className='bg-amber-100 rounded-lg p-3'>
+              <FaShoppingBag className='text-amber-800' />
             </div>
             <div className='pl-4'>
               <p className='text-gray-800 font-bold'>€{order.total}</p>
